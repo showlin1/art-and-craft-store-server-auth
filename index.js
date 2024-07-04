04ac96f10a2ddb89dc71e4595562d12d6dd6de21
unknown <showlinakter77@gmail.com>
2024-07-04 17:35:33 +0600

    updated art and craft card

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,18 +29,48 @@ async function run() {
         const artCollection = client.db('artAndCraftDB').collection('artAndCraft');
 
         // const userCollection = client.db
-        
-        app.get('/artAndCraft', async(req,res)=>{
+
+        app.get('/artAndCraft', async (req, res) => {
             const cursor = artCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.post('/artAndCraft', async(req,res)=>{
+        app.post('/artAndCraft', async (req, res) => {
             const newArtAndCraft = req.body;
             console.log(newArtAndCraft);
             const result = await artCollection.insertOne(newArtAndCraft);
             res.send(result);
+        })
+        app.get('/myArtAndCraft/:email', async (req, res) => {
+            console.log(req.params.email);
+            const result = await artCollection.find({ email: req.params.email }).toArray();
+            res.send(result);
+        })
+
+        app.put('/updateArtAndCraft/:_id', async (req, res) => {
+            const id = req.params._id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedArtAndCraft = req.body;
+            const artAndCraft = {
+                $set: {
+                    imageUrl: updatedArtAndCraft.imageUrl, itemName: updatedArtAndCraft.itemName, subcategoryName: updatedArtAndCraft.subcategoryName,
+                    description: updatedArtAndCraft.description, price: updatedArtAndCraft.price, rating: updatedArtAndCraft.rating,
+                    customization: updatedArtAndCraft.customization, processingTime: updatedArtAndCraft.processingTime,
+                    stockStatus: updatedArtAndCraft.stockStatus, email: updatedArtAndCraft.email, userName: updatedArtAndCraft.userName
+                }
+            }
+            const result = await artCollection.updateOne(filter,artAndCraft,options);
+            res.send(result);
+        })
+
+        app.get('/updateArtAndCraft/:_id', async (req, res) => {
+            console.log (req.params._id);
+            const result = await artCollection.findOne({ _id: new ObjectId(req.params._id) ,});
+            console.log(result);
+            res.send(result); 
         })
 
 
